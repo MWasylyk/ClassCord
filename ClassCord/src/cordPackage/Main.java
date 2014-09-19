e288aabf07a4d1746eba5543c470b3d3f47ff228
Michael Wasylyk <mikewas261@gmail.com>
2014-09-19 14:05:39 -0400

    Test update

--- ClassCord/src/cordPackage/Main.java
+++ ClassCord/src/cordPackage/Main.java
@@ -12,16 +12,15 @@ import java.util.TimerTask;
 public class Main {
 	static SeekBar seekBar = new SeekBar();
 	JFrame mainFrame = new JFrame();
+	
 	public static void main(String[] args) {
 		// Instantiate the JFrame object
 		@SuppressWarnings("unused")
 		Main m = new Main();
 		seekBar.setSeekLocation(0);
 		final Timer timer = new Timer();
-
-        // Test intellij ide git
+		
 		timer.scheduleAtFixedRate(new TimerTask() {
-			  @Override
 			  public void run() {
 				  if(seekBar.getSeekLocation()+1 < seekBar.getMaxTime()+1) {
 					  seekBar.setSeekLocation(seekBar.getSeekLocation()+1);
@@ -40,6 +39,9 @@ public class Main {
 	}
 	
 	public void init(){
+		//TODO Add play pause stop and record functionality and buttons 
+		JButton play = new JButton();
+		JButton record = new JButton();
 		// Panel for holding main JComponents
 		JPanel mainPanel = new JPanel();
 		mainFrame.setSize(600, 150);
@@ -49,19 +51,29 @@ public class Main {
 		mainPanel.setOpaque(true);
 		mainPanel.setBackground(Color.LIGHT_GRAY);
 		
+		play.setText("Play");
+		play.setSize(50, 30);
+		play.setLocation(0, 0);
+		
+		record.setText("Rec");
+		record.setSize(50,30);
+		record.setLocation(45, 0);
+		
 		seekBar.addMouseListener(seekBar);
 		seekBar.addMouseMotionListener(seekBar);
 		
 		// Set location of seekBar
-		seekBar.setLocation(25, 25);
+		seekBar.setLocation(25, 50);
 		
 		// Set size of seekBar
 		seekBar.setSize(550, 65);
-		seekBar.setMaxTime(120);
+		seekBar.setMaxTime(300);
 		//seekBar.setSeekLocation(150);
 		
 		// Add JComponents to the main panel and the JFrame
 		mainPanel.add(seekBar);
+		mainPanel.add(play);
+		mainPanel.add(record);
 		mainFrame.add(mainPanel);
 		mainFrame.setResizable(true);
 	}
